dd26085c7454b227d7c1dea8d18e883d09cb4bd5
minabyy <minabyy05@gmail.com>
2024-02-10 08:24:40 +0600

    pp2

--- lab2/lab_2.py
+++ lab2/lab_2.py
@@ -1,1 +1,31 @@
+#Python Booleans
 
+#Task 1
+print(10 > 9)
+#Answer: True
+
+#Task 2
+print(10 == 9)
+#Answer: False
+
+#Task 3
+print(10 < 9)
+#Answer: False
+
+#Task 4
+print(bool("abc"))
+#Answer: True
+
+#Task 5
+print(bool(0))
+#Answer: False
+
+#Operators
+#Task 1. Multiply 10 with 5, and print the result.
+print(10 * 5)
+#Result: 50
+
+#Task 2.
+Divide 10 by 2, and print the result.
+print(10 / 2)
+#Result: 5
